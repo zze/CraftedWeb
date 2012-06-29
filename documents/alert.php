5f5a61e753ecfbf7a91ca9b402eb9b1893f5d9c6
Faded <im@faded.me>
2012-06-29 00:17:44 -0700

    Updated: Default template Added: Fixed locations for the navigation menu and server status NOTE: Facebook connect does not currently work with this style update NOTE: Internet Explorer will not work with this style yet.

--- documents/alert.php
+++ documents/alert.php
@@ -23,8 +23,7 @@
 
 $alert_enabled = false;
 
-$alert_message = "We're currently doing some minor updates on the website. You may experience some parts disturbing in the meantime. <br/>
-				  We sincerely apologize for this.";
+$alert_message = "This is the alert demo.";
 
 ?>
 
