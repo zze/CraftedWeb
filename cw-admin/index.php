955498757fc23f51f68a5c6ee8d23f64b9d11f16
Faded <im@faded.me>
2012-07-01 11:42:32 -0700

    --> Testing branch commit

--- cw-admin/index.php
+++ cw-admin/index.php
@@ -1,3 +1,24 @@
+<?php
+/* ___           __ _           _ __    __     _     
+  / __\ __ __ _ / _| |_ ___  __| / / /\ \ \___| |__  
+ / / | '__/ _` | |_| __/ _ \/ _` \ \/  \/ / _ \ '_ \ 
+/ /__| | | (_| |  _| ||  __/ (_| |\  /\  /  __/ |_) |
+\____/_|  \__,_|_|  \__\___|\__,_| \/  \/ \___|_.__/ 
+
+		-[ Created by ©Nomsoft
+		  `-[ Original core by Anthony (Aka. CraftedDev)
+
+				-CraftedWeb Generation II-                  
+			 __                           __ _   							   
+		  /\ \ \___  _ __ ___  ___  ___  / _| |_ 							   
+		 /  \/ / _ \| '_ ` _ \/ __|/ _ \| |_| __|							   
+		/ /\  / (_) | | | | | \__ \ (_) |  _| |_ 							   
+		\_\ \/ \___/|_| |_| |_|___/\___/|_|  \__|	- www.Nomsoftware.com -	   
+                  The policy of Nomsoftware states: Releasing our software   
+                  or any other files are protected. You cannot re-release    
+                  anywhere unless you were given permission.                 
+                  © Nomsoftware 'Nomsoft' 2011-2012. All rights reserved.  */
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
   <head>
@@ -5,7 +26,7 @@
     <meta name='robots' content='all, follow' />
     <meta name="description" content="" />
     <meta name="keywords" content="" />
-    <title>Great admin</title>   
+    <title>CraftedWeb ACP</title>   
     <link href="public/css/login.css" rel="stylesheet" type="text/css" />
     <link href="public/css/login-blue.css" rel="stylesheet" type="text/css" />  <!-- color skin: blue / red / green / dark -->
   </head>
@@ -14,7 +35,7 @@
     <div id="content">
       <div id="login">
         
-        <div id="logo"><span>Great Admin</span></div>
+        <div id="logo"><span>CraftedWeb ACP</span></div>
                 
         <form method="post" action="login-exec.php" id="form-login" class="formBox">
           <fieldset>
