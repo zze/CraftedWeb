5b2a15abb4e2c6f3ce030f52d7f7ae02c6dbfdc1
Faded <im@faded.me>
2012-07-01 11:58:32 -0700

    -->Swapped mysql connection

--- cw-admin/login-exec.php
+++ cw-admin/login-exec.php
@@ -22,49 +22,53 @@
 <?php
 
 ini_set('display_errors', 1); ini_set('log_errors', 1); ini_set('error_log', dirname(__FILE__) . '/error_log.txt'); error_reporting(E_ALL);
-
 if(isset($_POST['submit'])) 
 {
-	
-	require('../config.php');
+	require('../../includes/configuration.php');
+	/*
 	mysql_connect($host, $user, $pass) or die(mysql_error());
-			
 	mysql_select_db($db);
-
 	$myusername = $_POST['username'];
-	
 	$mypassword = $_POST['password'];
-
-	$myusername = mysql_real_escape_string($myusername);
-	
+	$myusername = mysql_real_escape_string($myusername);	
 	$mypassword = mysql_real_escape_string($mypassword);
-
 	$result = mysql_query("SELECT * FROM users WHERE username='" . $myusername . "' AND password='" . sha1($mypassword) . "'") or die(mysql_error());
 
 	$r = mysql_fetch_array($result);
-	
 	$uid = $r['id'];
-
+	*/
+    $password = sha1("".$username.":".$password."");
+		 mysql_select_db($GLOBALS['connection']['logondb']);
+		 
+		 $result = mysql_query("SELECT COUNT(id) FROM account WHERE username='".$username."' AND 
+		 sha_pass_hash = '".$password."'");
+		 if(mysql_result($result,0)==0)
+			 die("Invalid username/password combination.");
+		 
+		 $getId = mysql_query("SELECT id FROM account WHERE username='".$username."'");
+		 $row = mysql_fetch_assoc($getId);
+		 $uid = $row['id'];
+		 $result = mysql_query("SELECT gmlevel FROM account_access WHERE id='".$uid."' 
+		 AND gmlevel >= '".$GLOBALS[$_POST['panel'].'Panel_minlvl']."'");
+		
+		 if(mysql_num_rows($result)==0)
+			 die("The specified account does not have access to log in!");
+			 
+			$r = mysql_fetch_array($result);
+			$uid = $r['id'];	 
+			
 	if(mysql_num_rows($result) == 1)
 	{
 		setcookie("craftedloggedin", 1, 0);
-		
 		setcookie("myusername", $myusername, 0);
-		
 		setcookie("uid", $uid, 0);
-		
 		if(isset($_COOKIE['login_fail'])) { setcookie("login_fail",FALSE); }
-		
 		header("Location: home.php");
 	}
 	else
 	{
-	
-		setcookie("login_fail", 1, time()+5);
-		
+		setcookie("login_fail", 1, time()+5);	
 		header("Location: index.php");
-		
-	
 	}
 }
 else
